15f9f56a77b691c0fbfbb3f508a1380fc657cb3f
Michael Demetriou <accounts@qzx.gr>
2019-08-13 15:29:37 +0300

    Some minor cleanup and commenting in main

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	// "github.com/go-fed/activity/streams"
 	"github.com/go-fed/activity/pub"
 	"github.com/gorilla/mux"
+
 	// "errors"
 	"log"
 	"net/http"
@@ -17,10 +18,8 @@ import (
 	"os"
 
 	"gopkg.in/ini.v1"
-
 	// "html"
 	// "context"
-
 	// "github.com/davecgh/go-spew/spew"
 )
 
@@ -30,32 +29,35 @@ func main() {
 
 	var err error
 
+	// This is here for debugging purposes. I want to be able to easily spot in the terminal
+	// when a single execution starts
 	fmt.Println("=========================================================================")
 
 	// read configuration file (config.ini)
-
 	cfg, err := ini.Load("config.ini")
-    if err != nil {
-        fmt.Printf("Fail to read file: %v", err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+	// config.ini for now only contains the baseURL
 	// Load base url from configuration file
 	baseURL = cfg.Section("general").Key("baseURL").String()
+	fmt.Println("Domain Name:", baseURL)
 
-    fmt.Println("Domain Name:", baseURL)
-
+	// This could work too if we don't want to handle multiple actors and stuff
+	// var outboxHandler http.HandlerFunc = actor.HandleOutbox
+	// might consider moving the whole thing to another file
 	var outboxHandler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
 		username := mux.Vars(r)["actor"]
-		// TODO replace this with a LoadActor that loads an actor from the database with this username
 		actor, err := LoadActor(username)
 		if err != nil {
 			fmt.Println("Can't create local actor")
 			return
 		}
-		if pub.IsActivityPubRequest(r){
+		if pub.IsActivityPubRequest(r) {
 			actor.HandleOutbox(w, r)
 		} else {
-			// The above does nothing if it's a non-ActivityPub request so 
+			// The above does nothing if it's a non-ActivityPub request so
 			// handle non-ActivityPub request here, such as serving a webpage.
 		}
 	}
@@ -68,10 +70,10 @@ func main() {
 			fmt.Println("Can't create local actor")
 			return
 		}
-		if pub.IsActivityPubRequest(r){
+		if pub.IsActivityPubRequest(r) {
 			actor.HandleInbox(w, r)
 		} else {
-			// The above does nothing if it's a non-ActivityPub request so 
+			// The above does nothing if it's a non-ActivityPub request so
 			// handle non-ActivityPub request here, such as serving a webpage.
 		}
 	}
@@ -99,15 +101,18 @@ func main() {
 	gorilla.HandleFunc("/{actor}/", actorHandler)
 	http.Handle("/", gorilla)
 
-	// get the list of users to relay
-	jsonFile, err := os.Open("actors.json")
+	// Here we begin the actual pherephone functionality
 
+	// get the list of local actors to host and the list
+	// of remote actors any of them relays (boosts, announces)
+	jsonFile, err := os.Open("actors.json")
 	if err != nil {
 		fmt.Println("something is wrong with the json file containing the actors")
 		fmt.Println(err)
 	}
 
 	// Unmarshall it into a map of string arrays
+	// TODO add summary thus making this map[string]interface{}
 	whoFollowsWho := make(map[string][]string)
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	json.Unmarshal(byteValue, &whoFollowsWho)
@@ -115,7 +120,8 @@ func main() {
 	// fmt.Println(string(byteValue))
 	// create all local actors if they don't exist yet
 	for follower, followees := range whoFollowsWho {
-		fmt.Println("Local Actor: "+ follower)
+		fmt.Println()
+		fmt.Println("Local Actor: " + follower)
 		followerActor, err := GetActor(follower, "emptySummary", "Service", baseURL+"/"+follower)
 		if err != nil {
 			fmt.Println("error creating local follower")
@@ -125,18 +131,8 @@ func main() {
 		fmt.Println("Users to relay:")
 		for _, followee := range followees {
 			fmt.Println(followee)
-			if err != nil{
-				fmt.Println("Couldn't create local actor")
-				return
-			}
 			followerActor.Follow(followee)
 		}
 	}
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
-
 	log.Fatal(http.ListenAndServe(":8081", nil))
-
 }
