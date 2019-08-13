f175c5987082c5459737b3853eb7c5a5761f7a11
Michael Demetriou <accounts@qzx.gr>
2019-08-13 16:28:02 +0300

    We can handle getting followed by others (at least if they send the same json we send, plus an id)
    Must see if we need to send id in the follow activities too

--- federatingBehavior.go
+++ federatingBehavior.go
@@ -3,18 +3,18 @@ package main
 import (
 	// "github.com/go-fed/activity/streams"
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
 
 	"github.com/go-fed/activity/pub"
 	"github.com/go-fed/activity/streams/vocab"
-
 	// "strings"
 )
 
 type federatingBehavior struct {
-	db *database
+	db     *database
 	parent *Actor
 }
 
@@ -26,16 +26,31 @@ func newFederatingBehavior(db *database) *federatingBehavior {
 
 func (f *federatingBehavior) PostInboxRequestBodyHook(c context.Context, r *http.Request, activity pub.Activity) (out context.Context, err error) {
 	fmt.Println("postinbox")
+
+	// it's a post of some kind, boost it
 	if activity.GetTypeName() == "Create" {
 		object := activity.GetActivityStreamsObject()
+		// assume it's an article
+		// TODO: check what it is and boost it anyway
 		article := object.Begin().GetActivityStreamsArticle()
 		id := article.GetActivityStreamsId()
 		fmt.Println(id)
 		f.parent.Announce(id.GetIRI().String())
-	} else {
-		//TODO check we aren't following ourselves
+	} else if activity.GetTypeName() == "Follow" {
+		// it's a follow, write it down
 		actor := activity.GetActivityStreamsActor()
 		newFollower := actor.Begin().GetIRI().String()
+		// check we aren't following ourselves
+		if newFollower == f.parent.iri {
+			fmt.Println("You can't follow yourself")
+			return out, errors.New("You can't follow yourself")
+		}
+		// check if this user is already following us
+		if _, ok := f.parent.followers[newFollower]; ok {
+			fmt.Println("You're already following us, yay!")
+			// do nothing, they're already following us
+			return
+		}
 		f.parent.JotFollowerDown(newFollower)
 	}
 	return
